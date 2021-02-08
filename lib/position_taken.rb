75f28cadee80c807ce9959544708278c2aade705
meaghan jennings <meaghanljennings@gmail.com>
2021-02-08 18:58:05 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 # code your #position_taken? method here!
-
 def position_taken?(board, index)
-  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-   return false
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
   else
-   return true
- end
-end 
+    return true
+  end 
+end
